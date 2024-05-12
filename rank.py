80d375401ce5efea5c777f19ec6ea12615328485
David Marques <david.2005.miguel@gmail.com>
2024-05-12 19:45:39 +0100

    finished. missing Queue from last week

--- rank.py
+++ rank.py
@@ -8,31 +8,38 @@ class Node:
 
 class BST:
     def __init__(self):
-        self.__root = Node() # Representa o nó da raíz da Binary Search Tree
+        self.__root = None # Representa o nó da raíz da Binary Search Tree
     
     # Associa o valor à chave na BST
     def put(self, key, val):
-        self.__root = self.put(self.__root, key, val)
+        self.__root = self.__put(self.__root, key, val)
 
     # Associa o valor à chave na BST. Se o nó já existir, substitui o seu valor pelo recebido
-    def put(self, x: Node, key, val):
+    def __put(self, x: Node, key, val):
         
         if x is None:
             return Node(key, val)
         if key == x.key:
             x.value = val
         elif key > x.key:
-            x.right = self.put(x.right, key, val)
+            x.right = self.__put(x.right, key, val)
         else: # key < x.key
-            x.left = self.put(x.left, key, val)
+            x.left = self.__put(x.left, key, val)
         x.count = 1 + self.size(x.left) + self.size(x.right)
 
     # Devolve a posição da chave key
     def rank(self, key):
-        pass
+        return self.__rank(key, self.__root)
     
-    def rank(self, key, x):
-        pass
+    def __rank(self, key, x):
+        if x is None:
+            return 0
+        if x.key == key:
+            return self.size(x.left)
+        elif key < x.key:
+            return self.__rank(key, x.left)
+        else: # key > x.key
+            return 1 + self.size(x.left) + self.__rank(key, x.right)
  
     # Devolve o valor double correspondente à chave recebida, ou 0 se não encontrar a chave
     def get(self, key):
@@ -47,27 +54,50 @@ class BST:
         return 0
     
     # Devolve o tamanho da BST
-    def size(self):
-        return self.__root.count
+    def size(self, x):
+        if x is None:
+            return 0
+        return x.count
 
     # Devolve a chave do nó k
     def select(self, k):
-        pass
+        return self.__select(self.__root, k)
 
     # Devolve o Nó que contém a key da classificação (rank) k
-    def select(self, x, k):
-        pass
+    def __select(self, x, k):
+        if x is None:
+            return None
+        left_size = self.size(x.left)
+        if left_size == k:
+            return x
+        elif left_size > k:
+            return self.__select(x.left, k)
+        else: #left_size < k
+            return self.__select(x.right, k - left_size - 1)
 
     # Iterador de chaves
     def keys(self):
         pass
+        q = Queue()
+        self.inorder(self.__root, q)
+        return q
     
     def inorder(self, x, q):
-        pass
+        if x is None:
+            return
+        self.inorder(x.left, q)
+        q.enqueue(x.key)
+        self.inorder(x.right, q)
 
     # Verifica se o rank e o select devolve a classificação e chave correta.
     def checkRankSelect(self):
-        pass
+        for i in range(self.size(self.__root)):
+            key = self.select(i).key
+            if i != self.rank(key):
+                return False
+            if key != self.select(self.rank(key)).key:
+                return False
+        return True
 
 def main():
     bst = BST()
